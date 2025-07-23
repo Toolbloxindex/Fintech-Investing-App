c7240f7c130cd6ac6d821afa8209fb46a6b78b2c
Thomas Correia <jfjsc.taba@gmail.com>
2025-07-23 14:27:09 +0100

    styled sign in page

--- fintech/app/signup.tsx
+++ fintech/app/signup.tsx
@@ -1,12 +1,16 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native'
 import React, { useState } from 'react'
 import { defaultStyles } from '@/constants/Styles'
 import Colors from '@/constants/Colors'
+import { Link } from 'expo-router'
 
 const Page = () => {
   const [countryCode, setCountryCode] = useState('+49')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const keyboardVerticalOffset = Platform.OS ==='ios' ? 80: 0
   const onSignup= async() => {}
   return (
+    <KeyboardAvoidingView style={{flex:1}} behavior='padding' keyboardVerticalOffset={keyboardVerticalOffset}>
     <View style={defaultStyles.container}>
       <Text style={defaultStyles.header}>Let's get started</Text>
       <Text style={defaultStyles.descriptionText}>
@@ -22,10 +26,25 @@ const Page = () => {
         <TextInput 
           style={[styles.input, {flex:1}]}
           placeholder="Mobile Number"
+          placeholderTextColor={Colors.gray}
           keyboardType="numeric"
+          value={phoneNumber}
+          onChangeText={setPhoneNumber}
         />
       </View>
+      <Link href={'/login'} replace asChild>
+        <TouchableOpacity>
+          <Text style={defaultStyles.textLink}>Already have an account? Log in</Text>
+        </TouchableOpacity>
+      </Link>
+      <View style={{flex:1}}/>
+      <TouchableOpacity style={[defaultStyles.pillButton,
+      phoneNumber !== '' ? styles.enabled : styles.disabled,
+        {marginBottom:20}]} onPress={onSignup}>
+        <Text style={defaultStyles.buttonText}>Sign up</Text>
+      </TouchableOpacity>
     </View>
+    </KeyboardAvoidingView>
   )
 }
 const styles = StyleSheet.create({
@@ -39,6 +58,12 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     fontSize: 20,
     marginRight: 10
+  },
+  enabled: {
+    backgroundColor: Colors.primary
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted
   }
 })
 
